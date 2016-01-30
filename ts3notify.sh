8096758f82be51d87f9bd879b9a19ed8c742b226
Splamy <splamyn@gmail.com>
2016-01-30 03:23:06 +0100

    New notify test

--- ts3notify.sh
+++ ts3notify.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+function EscTS() {
+  escTS=$1
+  escTS=${escTS//"\\"/"\\\\"}
+  escTS=${escTS//"/"/"\/"}
+  escTS=${escTS//" "/"\\s"}
+  escTS=${escTS//"|"/"\\p"}
+  escTS=${escTS//"\f"/"\\f"}
+  escTS=${escTS//"\n"/"\\n"}
+  escTS=${escTS//"\r"/"\\r"}
+  escTS=${escTS//"\t"/"\\t"}
+  escTS=${escTS//"\v"/"\\v"}
+}
+
+EscTS $TRAVIS_TEST_RESULT
+msg = escTS
+
 nc -q 2 87.106.2.173 10011 <<EOF
 use 1
-sendtextmessage targetmode=3 target=1 msg=Build\sfailed\syou\snabs
+sendtextmessage targetmode=3 target=1 msg=$msg
 quit
 EOF
