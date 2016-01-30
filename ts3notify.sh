90e4e999ad2471012d60a49e30825a25c81fccea
Splamy <splamyn@gmail.com>
2016-01-30 04:31:03 +0100

    Added summary

--- ts3notify.sh
+++ ts3notify.sh
@@ -2,15 +2,15 @@
 
 function EscTS() {
   escTS=$1
-  escTS=${escTS//"\\"/"\\\\"}
-  escTS=${escTS//"/"/"\\/"}
-  escTS=${escTS//" "/"\\s"}
-  escTS=${escTS//"|"/"\\p"}
-  escTS=${escTS//"\f"/"\\f"}
-  escTS=${escTS//"\n"/"\\n"}
-  escTS=${escTS//"\r"/"\\r"}
-  escTS=${escTS//"\t"/"\\t"}
-  escTS=${escTS//"\v"/"\\v"}
+  escTS=${escTS//$'\\'/$'\\\\'}
+  escTS=${escTS//$'/'/$'\\/'}
+  escTS=${escTS//$' '/$'\\s'}
+  escTS=${escTS//$'|'/$'\\p'}
+  escTS=${escTS//$'\f'/$'\\f'}
+  escTS=${escTS//$'\n'/$'\\n'}
+  escTS=${escTS//$'\r'/$'\\r'}
+  escTS=${escTS//$'\t'/$'\\t'}
+  escTS=${escTS//$'\v'/$'\\v'}
 }
 
 # check the the build result
@@ -26,10 +26,14 @@ commitCut=${TRAVIS_COMMIT:0:7}
 commitLink="[url=https://github.com/$TRAVIS_REPO_SLUG/commit/$TRAVIS_COMMIT]$commitCut[/url]"
 
 # now we do the same for the travis build
-buildLink="[url=https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID]Build $TRAVIS_BUILD_ID[/url]"
+buildLink="[url=https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID]Build #$TRAVIS_BUILD_NUMBER[/url]"
+
+# get git info
+gitTitle= git log --format=%B -n 1 HEAD
 
 # form a nice message
-EscTS "Commit $commitLink in branch $TRAVIS_BRANCH ($buildLink) $ok."
+lb=$'\n'
+EscTS "Commit $commitLink in branch $TRAVIS_BRANCH ($buildLink) $ok.$lb Summary:$gitTitle"
 
 nc -q 2 splamy.de 10011 <<EOF
 use 1
