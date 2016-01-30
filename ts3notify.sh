9a635f1dbd2369605af733c19c95bf7fa1d09505
Splamy <splamyn@gmail.com>
2016-01-30 04:08:10 +0100

    New format

--- ts3notify.sh
+++ ts3notify.sh
@@ -13,16 +13,27 @@ function EscTS() {
   escTS=${escTS//"\v"/"\\v"}
 }
 
+# check the the build result
 if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
   ok="succeeded"
 else
   ok="failed"
 fi
 
-EscTS "Commit $TRAVIS_COMMIT in branch $TRAVIS_BRANCH (Build $TRAVIS_BUILD_ID) $ok."
+# cut to the first 7 chars of the commit hash
+commitCut=${TRAVIS_COMMIT:0:7}
+# and set up a nice teamspeak link for it
+commitLink="[url=https://github.com/$TRAVIS_REPO_SLUG/commit/$TRAVIS_COMMIT]$commitCut[/url]"
 
-nc -q 2 87.106.2.173 10011 <<EOF
+# now we do the same for the travis build
+buildLink="[url=https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID]Build $TRAVIS_BUILD_ID[/url]"
+
+# form a nice message
+EscTS "Commit $commitLink in branch $TRAVIS_BRANCH ($buildLink) $ok."
+
+nc -q 2 splamy.de 10011 <<EOF
 use 1
+clientupdate client_nickname=TravisCI
 sendtextmessage targetmode=3 target=1 msg=$escTS
 quit
 EOF
