75014ecc1def8700f622b7bac45ff28c3339bcb3
Splamy <splamyn@gmail.com>
2018-06-11 18:04:24 +0200

    Matrix build

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TS3AudioBot.Helper.Environment;
+
+namespace TravisExperiments
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#if NET46
+				Console.WriteLine("built NET46");
+			#else
+				Console.WriteLine("built dotnet core");
+			#endif
+
+			Console.WriteLine("Version: {0}", SystemData.AssemblyData);
+			Console.WriteLine("Platform: {0}", SystemData.PlattformData);
+			Console.WriteLine("Runtime: {0}", SystemData.RuntimeData.FullName);
+		}
+	}
+}
