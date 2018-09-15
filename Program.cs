cfad8e92333c563e05f9bd890187d7b9a3f89422
Splamy <splamyn@gmail.com>
2018-09-15 19:55:10 +0200

    Test 2

--- Program.cs
+++ Program.cs
@@ -12,10 +12,6 @@ namespace TravisExperiments
 			#else
 				Console.WriteLine("built dotnet core");
 			#endif
-
-			Console.WriteLine("Version: {0}", SystemData.AssemblyData);
-			Console.WriteLine("Platform: {0}", SystemData.PlattformData);
-			Console.WriteLine("Runtime: {0}", SystemData.RuntimeData.FullName);
 		}
 	}
 }
